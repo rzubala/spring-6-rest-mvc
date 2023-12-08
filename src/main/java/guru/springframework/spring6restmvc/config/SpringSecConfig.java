0071b4949262abc2b6237af55bcd68869717be19
Rafał Zubala <rzubala@gmail.com>
2023-12-08 14:01:03 +0100

    openAPI rest assured

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,19 +2,21 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs**", "/swagger-ui/", "/swagger-ui/**", "/swagger-ui.html")
+                .requestMatchers("/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
